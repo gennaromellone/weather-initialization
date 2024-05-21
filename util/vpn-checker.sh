a15a299ebef408405ec166cb942e785820c97dba
Gennaro Mellone <mellone.gennaro@hotmail.com>
2024-05-21 16:33:43 +0200

    Update vpn-checker.sh

--- util/vpn-checker.sh
+++ util/vpn-checker.sh
@@ -1,4 +1,4 @@
-#!/bin/bash +x
+#!/bin/bash
 
 ISCONNECTED=1
 PREV_STATE=""
@@ -6,34 +6,35 @@ PREV_STATE=""
 while true; do
     sleep 30
 
-    # Controlla lo stato della connessione VPN
-    VPNCON=$(nmcli -f GENERAL.STATE con show id $HOSTNAME | awk '{print $2}')
-
-    if [[ $VPNCON == "activated" ]]; then
-        echo "Connected to VPN"
-        CURRENT_STATE="VPN"
-    elif [[ $VPNCON == "activating" ]]; then
-        echo "Not connected! Connecting to VPN"
-        CURRENT_STATE="CONNECTING"
+    # Verifica se la connessione è presente con un ping
+    PINGCON=$(ping -c2 -q -W 3 8.8.8.8 | grep "2 received")
+    if [[ $PINGCON != *"2 received"* ]]; then
+        echo "No connection detected, trying to reconnect..."
+        nmcli con down id $HOSTNAME
+        sleep 1s
+        nmcli con up id $HOSTNAME
+        CURRENT_STATE="DISCONNECTED"
     else
-        echo "VPN down"
-        # Controlla se la connessione LAN è attiva pingando un server esterno
-        PINGCON=$(ping -c2 -q -W 3 8.8.8.8 | grep "2 received")
-        if [[ $PINGCON != *"2 received"* ]]; then
-            echo "Disconnected, trying to reconnect…"
-            nmcli con down id $HOSTNAME
-            sleep 1s
-            nmcli con up id $HOSTNAME
-            CURRENT_STATE="DISCONNECTED"
+        echo "Connection detected"
+        
+        # Verifica lo stato della connessione VPN
+        VPNCON=$(nmcli -f GENERAL.STATE con show id $HOSTNAME | awk '{print $2}')
+
+        if [[ $VPNCON == "activated" ]]; then
+            echo "Connected to VPN"
+            CURRENT_STATE="VPN"
+        elif [[ $VPNCON == "activating" ]]; then
+            echo "Not connected! Connecting to VPN"
+            CURRENT_STATE="CONNECTING"
         else
-            echo "Connected to LAN!"
+            echo "VPN down, but LAN is connected"
             CURRENT_STATE="LAN"
         fi
     fi
 
     # Verifica se lo stato della connessione è cambiato
     if [[ "$PREV_STATE" != "$CURRENT_STATE" ]]; then
-        if [[  "$CURRENT_STATE" == "VPN" || "$CURRENT_STATE" == "LAN" || "$CURRENT_STATE" == "DISCONNECTED" ]]; then
+        if [[ "$CURRENT_STATE" == "LAN" || "$CURRENT_STATE" == "DISCONNECTED" ]]; then
             ISCONNECTED=0
         fi
     fi
