2bdc77cd07643b349422ad54fcb4ef4bd4dc85e0
Gennaro Mellone <mellone.gennaro@hotmail.com>
2024-05-21 15:45:14 +0200

    Update vpn-checker.sh

--- util/vpn-checker.sh
+++ util/vpn-checker.sh
@@ -4,6 +4,19 @@ while [ “true” ]
 do
 	sleep 30
 	VPNCON=$(nmcli -f GENERAL.STATE con show id $HOSTNAME | awk '{print $2}')
+
+
+
+ 	if [[ $VPNCON == "activated" ]]; then
+		echo "Connected to VPN"
+  	elif [[ $VPNCON == "activating" ]]; then
+   		echo "Not connected! Connecting to VPN"
+     	else
+      		echo "VPN down"
+	fi
+
+
+   
 	if [[ $VPNCON != "activated" ]]; then
 		echo "Disconnected, trying to reconnect…"
 		sleep 1s
